153a7194d9a1cf4c9aefadf1a1504c8dc5af3501
Artem <temychbeats@gmail.com>
2026-03-09 17:56:57 +0300

    Добавил обработку исключений для утилиты открытия файлов

--- code/fileimport.cpp
+++ code/fileimport.cpp
@@ -4,7 +4,7 @@
 #include <fstream>
 #include <sstream>
 
-void fileToConsole(std::string const& fileName) {
+void fileToConsole(const std::string& fileName) {
     /* Построчно выводит в консоль содержимое текстового файла */
 
     std::fstream curFile;
@@ -12,21 +12,29 @@ void fileToConsole(std::string const& fileName) {
 
     curFile.open(fileName);
 
-    if (curFile.is_open()) {
+    if (!curFile.is_open()) {
+        throw std::runtime_error("Не удаётся открыть файл: " + fileName);
+    }
+
+    try {
+
         std::cout << "=== Файл открыт: " << fileName <<" ===" << std::endl;
         while (std::getline(curFile, line)) {
             std::cout << line << std::endl;
         }
 
         curFile.close();
-        std::cout << "=== Файл закрыт! ===" << std::endl;
-    } else {
-        std::cerr << "Ошибка при открытии файла!" << std::endl;
+        std::cout << "=== Файл закрыт! ===\n" << std::endl;
+
+    } catch (...) {
+        throw std::runtime_error("Ошибка при открытии файла!");
     }
 }
 
-std::vector<std::vector<float>> fileImport(std::string const& fileName) {
-    /* Записывает данные файла (в формате .csv, т.е. построчно через запятую) в вектор векторов (std::vector) */
+std::vector<std::vector<float>> fileImport(const std::string& fileName) {
+    /* Записывает данные файла (в формате .csv, т.е. построчно через запятую)
+     * в двумерным массив (std::vector). Обрабатывает как файлы с метками классов (в виде целого числа),
+     * так и без метки. */
 
     std::fstream curFile;
     std::string line;
@@ -34,21 +42,30 @@ std::vector<std::vector<float>> fileImport(std::string const& fileName) {
 
     curFile.open(fileName);
 
-    if (curFile.is_open()) {
+    if (!curFile.is_open()) {
+        throw std::runtime_error("Не удаётся открыть файл: " + fileName);
+    }
+
+    try {
         while (std::getline(curFile, line)) {
+
+            if (line.empty() || line[0] == '#') {
+                continue;
+            }
+
             std::vector<float> row;
             std::stringstream ss(line);
-            std::string cord;
+            std::string num;
 
-            while (std::getline(ss, cord, ',')) {
-                row.push_back(std::stof(cord));
+            while (std::getline(ss, num, ',')) {
+                row.push_back(std::stof(num));
             }
 
             data.push_back(row);
+
         }
         return data;
-    } else {
-        std::cerr << "Ошибка при открытии файла!" << std::endl;
-        return data;
+    } catch (...) {
+        throw std::runtime_error("Ошибка при открытии файла!");
     }
 }
--- code/main.cpp
+++ code/main.cpp
@@ -8,11 +8,20 @@ int main() {
 
     /*
     ====== Пример использования утилиты открытия файла ======
+    */
 
     fileToConsole("../data/example.txt");
     fileToConsole("../data/example.csv");
 
     auto data = fileImport("../data/example.csv");
+    for (auto& row : data) {
+        std::cout << row[0] << " " << row[1] << std::endl;
+    }
+
+    /*
+    Пример выброса исключения
+
+    auto dataError = fileImport("../data/fileErroreExample.csv");
     for (auto& row : data) {
         std::cout << row[0] << " " << row[1] << std::endl;
     }
