589f4cb61c97525603baadb58dfdc57f5efc9b8d
Don1laFox <zhohovd2007@gmail.com>
2026-04-19 15:27:52 +0300

    Пример в main

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,11 @@
 #include "../include/neural_network.h"
 #include "../include/visualization.h"
 
-
 void runAllMatrixTests();
 void runAllFileImportTests();
 void runVisualizationTests();
 
 using namespace nnlab;
-
-
 
 int main(int argc, char* argv[]) {
 
@@ -23,7 +20,6 @@ int main(int argc, char* argv[]) {
     /*
      Для запуска демонстрации работы класса матриц и утилиты работы с файлами, надо просто запустить программу.
      Для запуска тестирования необходимо запустить программу с конфигурацией "--test".
-     Для запуска показа примера модели необходимо запустить программу с конфигурацией "--example".
     */
 
     bool runTests = false;
@@ -38,7 +34,6 @@ int main(int argc, char* argv[]) {
             break;
         }
     }
-
 
     if (runTests) {
         std::cout << "Matrix tests: \n" << std::endl;
@@ -74,78 +69,43 @@ int main(int argc, char* argv[]) {
         }
     }
     else if (exampleShow) {
-        auto [trainCoords, trainLabels] = genBinClassifyDataset(1000, 0.8,
-        0.4, 0.4,
-        0.6, 0.6
-        );
-        auto [testCoords, testLabels]   = genBinClassifyDataset(200, 0.8);
-
-        auto trainData = minMaxNormalization({trainCoords, trainLabels});
-        auto testData  = minMaxNormalization({testCoords, testLabels});
-
-        fileSaveToCSV("data/example_test.csv", testData.first, testData.second);
-
         NeuralNetwork default_model = NeuralNetwork();
-        NeuralNetwork custom_model = NeuralNetwork({2, 4, 8, 4, 1},
-            {relu, relu, relu, sigmoid},
-            {reluDerivative, reluDerivative, reluDerivative, sigmoidDerivative});
+        NeuralNetwork custom_model = NeuralNetwork(
+            {2, 4, 4, 1},
+            {relu,  relu, sigmoid},
+            {reluDerivative,  reluDerivative, sigmoidDerivative});
 
+        auto trainData = minMaxNormalization(genBinClassifyDataset(1000, 0.6));
+        auto testData = minMaxNormalization(genBinClassifyDataset(300, 0.6));
+
+        fileSaveToCSV("data/example.csv", testData.first, testData.second);
+        plot("data/example.csv");
 
         default_model.train(trainData);
-        custom_model.train(trainData,
-            bceLoss, bceDerivative,
+        custom_model.train(trainData, bceLoss, bceDerivative, 5000,
             0.01,
-            5000,
             1e-4,
-            15
-        );
+            20);
 
-        // === DEBUG: проверка весов ===
-        auto weights = custom_model.getWeights();
-        std::cout << "\n[DEBUG] Weight statistics:" << std::endl;
-        for (size_t l = 0; l < weights.size(); ++l) {
-            double maxW = 0, sumW = 0;
-            for (size_t i = 0; i < weights[l].rows(); ++i)
-                for (size_t j = 0; j < weights[l].cols(); ++j) {
-                    double w = std::abs(weights[l](i, j));
-                    maxW = std::max(maxW, w);
-                    sumW += w;
-                }
-            double avgW = sumW / (weights[l].rows() * weights[l].cols());
-            std::cout << "  Layer " << l << ": max|w|=" << maxW
-                      << ", avg|w|=" << avgW << std::endl;
-        }
+        auto weights = default_model.getWeights();
+        for (Matrix& i: weights) { std::cout << "\nLayer: \n" << i << std::endl; }
 
-        auto predictions_default = default_model.predict(testData.first);
-        auto predictions_custom  = custom_model.predict(testData.first);
+        auto test1 = default_model.predict(testData.first);
+        auto test2 = custom_model.predict(testData.first);
+        auto testProba1 = default_model.predictProba(testData.first);
+        auto testProba2 = custom_model.predictProba(testData.first);
 
-        auto probabilities_default = default_model.predictProba(testData.first);
-        auto probabilities_custom  = custom_model.predictProba(testData.first);
+        std::cout << "\nProbability test for default model:" << std::endl;
+        for (int i = 0; i < test1.size(); i = i + 20) { std::cout << test1[i] << " " << testProba1[i] << std::endl; }
 
-        double acc_default = accuracy(predictions_default, testData.second);
-        double acc_simple  = accuracy(predictions_custom, testData.second);
+        std::cout << "\nProbability test for custom model:" << std::endl;
+        for (int i = 0; i < test2.size(); i = i + 20) { std::cout << test2[i] << " " << testProba2[i] << std::endl; }
 
-        double auc_default = rocAuc(probabilities_default, testData.second);
-        double auc_simple  = rocAuc(probabilities_custom, testData.second);
 
-        std::cout << "\n RESULTS" << std::endl;
-
-        std::cout << "\n Default model (2-4-4-1, MSE):" << std::endl;
-        std::cout << "   Accuracy: " << acc_default << std::endl;
-        std::cout << "   ROC-AUC:  " << auc_default << std::endl;
-
-        std::cout << "\n Custom model (2-4-8-4-1, BCE, relu):" << std::endl;
-        std::cout << "   Accuracy: " << acc_simple << std::endl;
-        std::cout << "   ROC-AUC:  " << auc_simple << std::endl;
-
-        std::cout << "Example of predictions (10 first points):" << std::endl;
-        std::cout << "№\tPrediction\tProbability\tTrue class" << std::endl;
-        for (int i = 0; i < 10; ++i) {
-            std::cout << i << "\t"
-                      << predictions_custom[i] << "\t\t"
-                      << probabilities_custom[i] << "\t\t"
-                      << testData.second[i] << std::endl;
-        }
+        std::cout << "\nAccuracy: \nnet1: " << accuracy(test1, testData.second) <<
+            "\nnet2: " << accuracy(test2, testData.second) << std::endl;
+        std::cout << "\nRoc-Auc: \nnet1: " << rocAuc(testProba1, testData.second) <<
+            "\nnet2: " << rocAuc(testProba2, testData.second) << std::endl;
     }
     else{
 
@@ -181,7 +141,7 @@ int main(int argc, char* argv[]) {
         std::cout << "\nRoc-Auc: \nnet1: " << rocAuc(testProba1, testData.second) <<
             "\nnet2: " << rocAuc(testProba2, testData.second) << std::endl;
 
+
         return 0;
     }
 }
-
