14a2c621138245f6cff4af7a53f05265fa749ab5
Don1laFox <zhohovd2007@gmail.com>
2026-04-19 15:16:20 +0300

    Пример в main

--- src/main.cpp
+++ src/main.cpp
@@ -23,9 +23,14 @@ int main(int argc, char* argv[]) {
     */
 
     bool runTests = false;
+    bool exampleShow = false;
     for (int i = 1; i < argc; i++) {
         if (std::string(argv[i]) == "--test") {
             runTests = true;
+            break;
+        }
+        else if (std::string(argv[i]) == "--example") {
+            exampleShow = true;
             break;
         }
     }
@@ -62,6 +67,45 @@ int main(int argc, char* argv[]) {
             std::cerr << "Unknown error" << std::endl;
             return 1;
         }
+    }
+    else if (exampleShow) {
+        NeuralNetwork default_model = NeuralNetwork();
+        NeuralNetwork custom_model = NeuralNetwork(
+            {2, 4, 4, 1},
+            {relu,  relu, sigmoid},
+            {reluDerivative,  reluDerivative, sigmoidDerivative});
+
+        auto trainData = minMaxNormalization(genBinClassifyDataset(1000, 0.6));
+        auto testData = minMaxNormalization(genBinClassifyDataset(300, 0.6));
+
+        fileSaveToCSV("data/example.csv", testData.first, testData.second);
+        plot("data/example.csv");
+
+        default_model.train(trainData);
+        custom_model.train(trainData, bceLoss, bceDerivative, 5000,
+            0.01,
+            1e-4,
+            20);
+
+        auto weights = default_model.getWeights();
+        for (Matrix& i: weights) { std::cout << "\nLayer: \n" << i << std::endl; }
+
+        auto test1 = default_model.predict(testData.first);
+        auto test2 = custom_model.predict(testData.first);
+        auto testProba1 = default_model.predictProba(testData.first);
+        auto testProba2 = custom_model.predictProba(testData.first);
+
+        std::cout << "\nProbability test for default model:" << std::endl;
+        for (int i = 0; i < test1.size(); i = i + 20) { std::cout << test1[i] << " " << testProba1[i] << std::endl; }
+
+        std::cout << "\nProbability test for custom model:" << std::endl;
+        for (int i = 0; i < test2.size(); i = i + 20) { std::cout << test2[i] << " " << testProba2[i] << std::endl; }
+
+
+        std::cout << "\nAccuracy: \nnet1: " << accuracy(test1, testData.second) <<
+            "\nnet2: " << accuracy(test2, testData.second) << std::endl;
+        std::cout << "\nRoc-Auc: \nnet1: " << rocAuc(testProba1, testData.second) <<
+            "\nnet2: " << rocAuc(testProba2, testData.second) << std::endl;
     }
     else{
 
