d6acdbed19e2209e50a2945b5c94ddf843018b68
Don1laFox <zhohovd2007@gmail.com>
2026-05-19 19:16:49 +0300

    bugfix + обновление README

--- scripts/nnlab.py
+++ scripts/nnlab.py
@@ -3,46 +3,66 @@ import nnlab_py
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-import random
-
-#билд проекта
-# cd C:\Users\yesia\CLionProjects\NeuroForge
-# Remove-Item -Recurse -Force build
-# mkdir build
-# cd build
-# cmake .. -G "Visual Studio 17 2022"
-# cmake --build . --config Release
 
 
-file_id = input("\n Enter file's id from google drive: \n")
-url = f'https://drive.google.com/uc?export=download&id={file_id}'
+flag = input("\nEnter 1 for 1st dataset, 2 for 2nd or smth else for another: \n")
 
-data = pd.read_csv(url)
+if flag == '1':
+    model = nnlab_py.NeuralNetwork(
+        structure=[2, 16, 16, 1],
+        activations=[nnlab_py.tanh, nnlab_py.tanh, nnlab_py.sigmoid],
+        activations_deriv=[nnlab_py.tanhDerivative, nnlab_py.tanhDerivative, nnlab_py.sigmoidDerivative]
+    )
+    url = 'https://drive.google.com/uc?export=download&id=16VzwtrZRP9QScMdueb3BK0fvZtP8a6sH'
+    data = pd.read_csv(url)
+elif flag == '2':
+    model = nnlab_py.NeuralNetwork(
+        structure=[4, 48, 64, 32, 1],
+        activations=[nnlab_py.tanh, nnlab_py.tanh, nnlab_py.tanh, nnlab_py.sigmoid],
+        activations_deriv=[nnlab_py.tanhDerivative, nnlab_py.tanhDerivative, nnlab_py.tanhDerivative, nnlab_py.sigmoidDerivative]
+    )
+    url = 'https://drive.google.com/uc?export=download&id=13npPTtGGiKyug7fEKyvaZZbFavFkhGLm'
+    data = pd.read_csv(url)
+else:
+    file_id = input("\n Enter file's id from google drive: \n")
+    url = f'https://drive.google.com/uc?export=download&id={file_id}'
+    data = pd.read_csv(url)
+
+    struc = [data.shape[1] - 1] + list(map(int, input("Enter layers from 2 to N as: \n l2 l3 l4 ... \n ").split()))
+    act = []
+    act_der = []
+    print('Let`s make list of activation funcs: \n')
+    for i in range(len(struc) - 1):
+        func = input("Enter 1 for tanh, 2 for relu, smth else for sigmoid: \n")
+        if func == '1':
+            act.append(nnlab_py.tanh)
+            act_der.append(nnlab_py.tanhDerivative)
+        elif func == '2':
+            act.append(nnlab_py.relu)
+            act_der.append(nnlab_py.reluDerivative)
+        else:
+            act.append(nnlab_py.sigmoid)
+            act_der.append(nnlab_py.sigmoidDerivative)
+
+    model = nnlab_py.NeuralNetwork(structure=struc, activations=act, activations_deriv=act_der)
+
+
+print("Model is created")
+
 
 target_column = 'target'
 X = data.drop(target_column, axis=1)
 y = data[target_column]
 
-# X, y = nnlab_py.min_max_normalization(X, y)
+X_raw = np.ascontiguousarray(X, dtype=np.float64)
+y_raw = np.ascontiguousarray(y, dtype=np.int32)
+
+X, y = nnlab_py.min_max_normalization(X_raw, y_raw)
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42,
 )
-## датасет 2
-model = nnlab_py.NeuralNetwork(
-    structure=[4, 48, 64, 32, 1],
-    activations=[nnlab_py.tanh, nnlab_py.tanh, nnlab_py.tanh, nnlab_py.sigmoid],
-    activations_deriv=[nnlab_py.tanhDerivative, nnlab_py.tanhDerivative, nnlab_py.tanhDerivative, nnlab_py.sigmoidDerivative]
-)
 
-
-# датасет 1
-# model = nnlab_py.NeuralNetwork(
-#     structure=[2, 16, 16, 1],
-#     activations=[nnlab_py.tanh, nnlab_py.tanh, nnlab_py.sigmoid],
-#     activations_deriv=[nnlab_py.tanhDerivative, nnlab_py.tanhDerivative, nnlab_py.sigmoidDerivative]
-# )
-print("Model is created")
 
 
 model.train(X_train, y_train,
